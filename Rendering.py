b8db6886b856360603a87ea6846969056734a9f0
jadenbaekim <jadenbaekim@gmail.com>
2025-09-02 12:38:43 -0400

    Update Rendering.py

--- Rendering.py
+++ Rendering.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 2 * np.pi, 200)
-y = np.sin(x)
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+from BruteForce import BruteForceSolution
+from random import randint
+points = []
+for x in range(0, 10):
+    points.append([random.randint(0,1000),random.randint(0,1000)])
+solution = BruteForceSolution(points)
+def plotPoints(points):

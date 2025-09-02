9283974c313c169344b34433d36c035302917b27
Justin9734 <113266908+Justin9734@users.noreply.github.com>
2025-09-02 12:03:16 -0400

    Update BruteForce.py

--- BruteForce.py
+++ BruteForce.py
@@ -1,1 +1,20 @@
-print("hi")
+from math import *
+import itertools
+
+def FindDistance(point1, point2): # idc abt lambda
+    distance = sqrt(abs(point1[0]-point2[0])**2 + abs(point1[1]-point2[1])**2)
+    return distance
+
+def BruteForceSolution(points):
+    paths = itertools.combinations(points, len(points))
+    distances = []
+    for path in paths:
+        distance = 0
+        for point in range(0, len(path)):
+            try:
+                distance += FindDistance(path[point], path[point+1])
+            except:
+                pass
+        distances.append(distance)
+    return [min(distances), paths[distances.index(min(distances))]]
+        
